a6f495e97eb3663ad2ee41d69058caf0f49d7ba7
Joshua Cox <joshuajosephcox@gmail.com>
2024-06-20 10:58:49 +0100

    Create initial welcome screen
    Remove 'App.css' as all CSS will be written in 'index.css'
    
    Remove React inital files 'logo.svg', 'reportWebVitals.js' and
    'setupTests.js' - they won't be used.
    
    Create a simple welcome message.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Home from './home/Home';
+
+function App() {
+  return (
+    <div className='App'>
+
+      <div className='content'>
+
+        <Home />
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className='Home'>
+
+        <h1>Hi. I'm Josh</h1>
+
+      </div>
+    );
+  }
+  
+  export default Home;
